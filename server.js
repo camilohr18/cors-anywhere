de319bdce87639487a1a616c59a87b52718cbfe6
camilohr18 <39716385+camilohr18@users.noreply.github.com>
2023-08-25 10:24:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['https://bizerbapayments.com'],
+  originWhitelist: ['https://bizerbapayments.com', 'http://localhost:4200'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
